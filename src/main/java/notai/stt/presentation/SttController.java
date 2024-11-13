b88d3681b0834b8b67ede9970289f079f5504899
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-14 01:27:15 +0900

    feat: STT 결과에 소유검증 추가

--- src/main/java/notai/stt/presentation/SttController.java
+++ src/main/java/notai/stt/presentation/SttController.java
@@ -1,6 +1,7 @@
 package notai.stt.presentation;
 
 import lombok.RequiredArgsConstructor;
+import notai.auth.Auth;
 import notai.stt.application.SttQueryService;
 import notai.stt.presentation.response.SttPageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,10 @@ public class SttController {
 
     @GetMapping("/documents/{documentId}/pages/{pageNumber}")
     public SttPageResponse getSttByPage(
+            @Auth Long memberId,
             @PathVariable Long documentId,
             @PathVariable Integer pageNumber
     ) {
-        return sttQueryService.getSttByPage(documentId, pageNumber);
+        return sttQueryService.getSttByPage(memberId, documentId, pageNumber);
     }
 }
