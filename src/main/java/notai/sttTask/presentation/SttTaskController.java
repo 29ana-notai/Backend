3cdcbff24efd4373cad7739955b21b255fbd3251
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-14 01:39:05 +0900

    Chore: SttTaskController base endpoint추가

--- src/main/java/notai/sttTask/presentation/SttTaskController.java
+++ src/main/java/notai/sttTask/presentation/SttTaskController.java
@@ -8,9 +8,11 @@ import notai.sttTask.presentation.response.SttTaskOverallStatusResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/ai/stt")
 @RestController
 public class SttTaskController {
 
